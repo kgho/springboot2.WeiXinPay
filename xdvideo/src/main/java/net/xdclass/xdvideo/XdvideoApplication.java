b8215f2bf79e3e91817bf55ab954d2a367fcd719
kgho <kuanggh@outlook.com>
2020-10-14 10:15:26 +0800

    创建

---
+++ xdvideo/src/main/java/net/xdclass/xdvideo/XdvideoApplication.java
@@ -0,0 +1,13 @@
+package net.xdclass.xdvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdvideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XdvideoApplication.class, args);
+	}
+
+}
