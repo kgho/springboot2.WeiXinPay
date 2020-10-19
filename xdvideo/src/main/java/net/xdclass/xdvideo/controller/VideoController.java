97009c2db49e60833d53f3fc25989cc783f5c217
kgho <kuanggh@outlook.com>
2020-10-19 16:19:07 +0800

    13.使用Mybatis注解开发视频列表增删改查

--- xdvideo/src/main/java/net/xdclass/xdvideo/controller/VideoController.java
+++ xdvideo/src/main/java/net/xdclass/xdvideo/controller/VideoController.java
@@ -1,10 +1,50 @@
 package net.xdclass.xdvideo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.xdclass.xdvideo.domain.Video;
+import net.xdclass.xdvideo.service.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/video")
 public class VideoController {
 
+    @Autowired
+    private VideoService videoService;
+
+    @GetMapping("page")
+    public Object pageVideo(){
+
+        return videoService.findAll();
+    }
+
+    @GetMapping("find_by_id")
+    public Object findById(int videoId){
+
+        return videoService.findById(videoId);
+    }
+
+    @DeleteMapping("del_by_id")
+    public Object delById(int videoId){
+
+        return videoService.delete(videoId);
+    }
+
+    @PutMapping("update_by_id")
+    public Object update(int videoId, String title){
+
+        Video video = new Video();
+        video.setId(videoId);
+        video.setTitle(title);
+        return videoService.update(video);
+    }
+
+    @PostMapping("save")
+    public Object save(String title){
+
+        Video video = new Video();
+        video.setTitle(title);
+        return videoService.save(video);
+    }
 
 }
--- xdvideo/src/main/java/net/xdclass/xdvideo/service/VideoService.java
+++ xdvideo/src/main/java/net/xdclass/xdvideo/service/VideoService.java
@@ -1,5 +1,22 @@
 package net.xdclass.xdvideo.service;
 
-public class VideoService {
+import net.xdclass.xdvideo.domain.Video;
+
+import java.util.List;
+
+/**
+ * 视频业务类接口
+ */
+public interface VideoService {
+
+    List<Video> findAll();
+
+    Video findById(int id);
+
+    int update(Video Video);
+
+    int delete(int id);
+
+    int save(Video video);
 
 }
