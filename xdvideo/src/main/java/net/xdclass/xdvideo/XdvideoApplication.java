97c1ece3b561b28d9104bea70bbe90715e82ee04
kgho <kuanggh@outlook.com>
2020-10-19 14:49:37 +0800

    12.整合Mybatis访问数据库和阿里巴巴数据源

--- xdvideo/src/main/java/net/xdclass/xdvideo/XdvideoApplication.java
+++ xdvideo/src/main/java/net/xdclass/xdvideo/XdvideoApplication.java
@@ -1,8 +1,10 @@
 package net.xdclass.xdvideo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("net.xdclass.xdvideo.mapper")
 @SpringBootApplication
 public class XdvideoApplication {
 
