6602b4c086944e2cb690dbf5e96183cc7bfbf947
kgho <kuanggh@outlook.com>
2020-10-21 10:07:58 +0800

    28.使用JWT生成用户Token回写客户端

--- xdvideo/src/main/java/net/xdclass/xdvideo/controller/WechatController.java
+++ xdvideo/src/main/java/net/xdclass/xdvideo/controller/WechatController.java
@@ -4,6 +4,7 @@ import net.xdclass.xdvideo.config.WeChatConfig;
 import net.xdclass.xdvideo.domain.JsonData;
 import net.xdclass.xdvideo.domain.User;
 import net.xdclass.xdvideo.service.UserService;
+import net.xdclass.xdvideo.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -44,9 +46,16 @@ public class WechatController {
         return JsonData.buildSuccess(qrcodeUrl);
     }
 
+    /**
+     * 微信扫码登录，回调地址
+     * @param code
+     * @param state
+     * @param response
+     * @throws IOException
+     */
     @GetMapping("/user/callback")
     public void wechatUserCallback(@RequestParam(value = "code",required = true) String code,
-                                   String state, HttpServletResponse response){
+                                   String state, HttpServletResponse response) throws IOException {
 
         //System.out.println("code="+code);
         //System.out.println("state="+state);
@@ -56,7 +65,10 @@ public class WechatController {
         User user = userService.saveWeChatUser(code);
         if(user != null){
             //生成jwt
+            String token = JwtUtils.geneJsonWebToken(user);
+            // state 当前用户的页面地址，需要拼接 http://  这样才不会站内跳转
 
+            response.sendRedirect(state+"?token="+token+"&head_img="+user.getHeadImg()+"&name="+URLEncoder.encode(user.getName(),"UTF-8"));
         }
     }
 
