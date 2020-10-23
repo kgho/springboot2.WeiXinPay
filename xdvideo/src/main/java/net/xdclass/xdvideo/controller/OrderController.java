0ebdc65d563fd6df537b5093529759aec83dc75f
kgho <kuanggh@outlook.com>
2020-10-23 15:07:52 +0800

    41.谷歌二维码工具生成扫一扫支付二维码

--- xdvideo/src/main/java/net/xdclass/xdvideo/controller/OrderController.java
+++ xdvideo/src/main/java/net/xdclass/xdvideo/controller/OrderController.java
@@ -1,5 +1,11 @@
 package net.xdclass.xdvideo.controller;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import net.xdclass.xdvideo.domain.JsonData;
 import net.xdclass.xdvideo.dto.VideoOrderDto;
 import net.xdclass.xdvideo.service.VideoOrderService;
@@ -11,6 +17,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 订单接口
@@ -25,8 +35,9 @@ public class OrderController {
     private VideoOrderService videoOrderService;
 
     @GetMapping("add")
-    public JsonData saveOrder(@RequestParam(value = "video_id",required = true)int videoId,
-                              HttpServletRequest request) throws Exception {
+    public void saveOrder(@RequestParam(value = "video_id",required = true)int videoId,
+                              HttpServletRequest request,
+                              HttpServletResponse response) throws Exception {
 
         //String ip = IpUtils.getIpAddr(request);
         //int userId = request.getAttribute("user_id");
@@ -38,10 +49,29 @@ public class OrderController {
         videoOrderDto.setIp(ip);
 
         String codeUrl =videoOrderService.save(videoOrderDto);
+        if(codeUrl == null) {
+            throw new  NullPointerException();
+        }
 
-        //生成二维码
 
-        return JsonData.buildSuccess("下单成功");
+        try{
+            //生成二维码配置
+            Map<EncodeHintType,Object> hints =  new HashMap<>();
+
+            //设置纠错等级
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            //编码类型
+            hints.put(EncodeHintType.CHARACTER_SET,"UTF-8");
+
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(codeUrl, BarcodeFormat.QR_CODE,400,400,hints);
+            OutputStream out =  response.getOutputStream();
+
+            MatrixToImageWriter.writeToStream(bitMatrix,"png",out);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
 
